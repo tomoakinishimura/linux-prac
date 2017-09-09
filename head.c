3cc9a83451138cdb25ef918942cbc350a8bf1857
tomoakinishimura <n.t0226@gmail.com>
2017-09-09 07:08:08 +0000

    コマンド引数対応。-h:help

--- head.c
+++ head.c
@@ -1,24 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define _GNU_SOURCE
+#include <getopt.h>
+
 static void do_head(FILE *f, long nlines);
+
+#define DEFAULT_N_LINES 10
+
+static struct option longopts[] = {
+  {"lines", required_argument, NULL, 'n'},
+  {"help", no_argument, NULL, 'h'},
+  {0, 0, 0, 0}
+};
 
 int main(int argc, char *argv[])
 {
-  long nlines; 
-  
-  if(argc < 2){
-    fprintf(stderr, "Usage: %s n\n", argv[0]);
-    exit(1);
+  int opt;
+  long nlines = DEFAULT_N_LINES;
+
+  while((opt = getopt_long(argc, argv, "n:", longopts, NULL)) != -1){
+    switch(opt) {
+      case 'n':
+        nlines = atoi(optarg);
+        break;
+      case 'h':
+        fprintf(stdout, "Usage: %s [-n LINES] [FILE ...]\n", argv[0]);
+        exit(0);
+      case '?':
+        fprintf(stderr, "Usage: %s [-n LINES] [FILE ...]\n", argv[0]);
+        exit(1);
+    }
   }
 
-  nlines = atol(argv[1]);
-  if(argc == 2){
+  if(optind == argc){
     do_head(stdin, nlines);
   }else{
     int i;
 
-    for(i = 2; i < argc; i++){
+    for(i = optind; i < argc; i++){
       FILE *f;
 
       f = fopen(argv[i], "r");
